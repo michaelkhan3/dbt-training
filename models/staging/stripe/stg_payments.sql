98aa40c7febe68bb3305c1cf9af529457df5eab4
Michael Khan <michaelkhan31@gmail.com>
2024-03-13 07:40:51 +0000

    Completed models section

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,10 @@
+select 
+  id as payment_id,
+  orderid as order_id,
+  paymentmethod as payment_method,
+  status,
+  amount,
+  created,
+  _batched_at as batched_at
+
+from `michael-khan-sandbox.stripe.payment`
