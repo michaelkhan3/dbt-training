fa9bb08425f83102b088f5cc8954809172bdf3b5
Michael Khan <michaelkhan31@gmail.com>
2024-03-14 02:13:34 +0000

    Add source for jaffle_shop tables

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,4 +3,4 @@ select
     first_name,
     last_name
 
-from `michael-khan-sandbox.jaffle_shop.customers`
+from {{ source('jaffle_shop', 'customers') }}
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,4 +4,4 @@ select
     order_date,
     status
 
-from `michael-khan-sandbox.jaffle_shop.orders`
+from {{ source('jaffle_shop', 'orders') }}
